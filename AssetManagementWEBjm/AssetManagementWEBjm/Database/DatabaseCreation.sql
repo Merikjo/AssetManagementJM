28669f8190877e0eb12738b4fa37040285b890e4
Johanna Merikanto <johanna.merikanto@stud.pointcollege.fi>
2016-04-07 17:14:20 +0300

    Lisätty tietokannan luontikoodi eriliseen SQL-tiedostoon. (JM)

---
+++ AssetManagementWEBjm/AssetManagementWEBjm/Database/DatabaseCreation.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[AssetLocation]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Code] NVARCHAR(100),
+	[Name] NVARCHAR(200),
+	[Adress] NVARCHAR(500)
+	)
+	CREATE TABLE [dbo].[Assets]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Code] NVARCHAR(100),
+	[Type] NVARCHAR(500),
+	[Model] NVARCHAR(500)
+	)
+	CREATE TABLE [dbo].[AssetLocations]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[LocationId] INT,
+	[AssetId] INT,
+	[LastSeen] DATETIME
+	)
