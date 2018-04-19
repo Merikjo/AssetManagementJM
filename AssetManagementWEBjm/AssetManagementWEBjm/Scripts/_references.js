75c234fa7d877dc713af10aefcbc8ecb263e63a9
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-04-19 08:15:48 +0300

    Päivitetty 19.4.2018 sovelluksen nuget -paketteja.

--- AssetManagementWEBjm/AssetManagementWEBjm/Scripts/_references.js
+++ AssetManagementWEBjm/AssetManagementWEBjm/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-3.2.1.js" />
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <reference path="modernizr-2.8.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="ai.0.15.0-build58334.min.js" />
 /// <reference path="application/logic.js" />
